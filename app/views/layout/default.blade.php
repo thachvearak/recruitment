87904ed255393683c6094caeb1ebc26b6a9b9ede
cuoool <chea.bonnak@gmail.com>
2014-11-24 08:20:00 -0800

    create default main layout

---
+++ app/views/layout/default.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	
+	<!-- CSS -->
+	<link rel="stylesheet" href="{{asset('assets/css/jquery.mobile.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/jquery-ui.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap/bootstrap-theme.min.css')}}">
+	
+	<!-- JS -->
+	<script src="{{asset('assets/js/jquery-2.1.1.min.js')}}"></script>
+	<script src="{{asset('assets/js/jquery-ui.min.js')}}"></script>
+	<script src="{{asset('assets/js/jquery.mobile.min.js')}}"></script>	
+	<script src="{{asset('assets/js/bootstrap/bootstrap.min.js')}}"></script>
+</head>
+<body>
+	@yield('header')
+</body>
+</html>
