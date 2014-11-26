7553092311f60dc1e497ac2dca626c302a7e7727
cuoool <chea.bonnak@gmail.com>
2014-11-26 09:15:07 -0800

    Create menu bar

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -2,9 +2,15 @@
 	<div>
 	</div>
 	<div class="menu">
-		<ul>
+		<ul class="nav nav nav-pills" >
 			<li><a href="">Home</a></li>
-			<li><a href="">Job Seeker</a></li>
+			<li><a href="">Job Seeker</a>
+				<ul class="dropdown-menu">
+					<li><a href="">Create CV</a></li>
+					<li><a href="">Search job</a></li>
+					<li><a href="">Question & Answer</a></li>
+				</ul>
+			</li>
 			<li><a href="">Our Service</a></li>
 			<li><a href="">Resource</a></li>
 		</ul>
