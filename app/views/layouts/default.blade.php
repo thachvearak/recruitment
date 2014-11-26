3bebcb19002e8f9353563925a1fc4d243169911b
cuoool <chea.bonnak@gmail.com>
2014-11-26 07:05:49 -0800

    update layout

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -5,15 +5,15 @@
 	<title></title>
 	
 	<!-- CSS -->
-	<link rel="stylesheet" href="{{asset('assets/css/jquery.mobile.min.css')}}">
-	<link rel="stylesheet" href="{{asset('assets/css/jquery-ui.css')}}">
+	<!--  <link rel="stylesheet" href="{{asset('assets/css/jquery.mobile.min.css')}}"> -->
+	<!--  <link rel="stylesheet" href="{{asset('assets/css/jquery-ui.css')}}"> -->
 	<link rel="stylesheet" href="{{asset('assets/css/bootstrap/bootstrap.min.css')}}">
 	<link rel="stylesheet" href="{{asset('assets/css/bootstrap/bootstrap-theme.min.css')}}">
 	
 	<!-- JS -->
 	<script src="{{asset('assets/js/jquery-2.1.1.min.js')}}"></script>
-	<script src="{{asset('assets/js/jquery-ui.min.js')}}"></script>
-	<script src="{{asset('assets/js/jquery.mobile.min.js')}}"></script>	
+	<!--  <script src="{{asset('assets/js/jquery-ui.min.js')}}"></script> -->
+	<!--  <script src="{{asset('assets/js/jquery.mobile.min.js')}}"></script> -->	
 	<script src="{{asset('assets/js/bootstrap/bootstrap.min.js')}}"></script>
 </head>
 <body>
--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -1,2 +1,12 @@
 <div class="container header">
+	<div>
+	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Job Seeker</a></li>
+			<li><a href="">Our Service</a></li>
+			<li><a href="">Resource</a></li>
+		</ul>
+	</div>
 </div>
