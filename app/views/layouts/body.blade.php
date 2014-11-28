4e19e9ebf9871ccfc97d38f3d4807c9f3d9ac74e
cuoool <chea.bonnak@gmail.com>
2014-11-28 08:53:27 -0800

    Create body file

---
+++ app/views/layouts/body.blade.php
@@ -0,0 +1,91 @@
+<div class="container body" id="search-block" >	
+	<div class="row">
+		<div class="form-group col-md-12">
+			<label for="search-keyword">Keywords</label>				
+			<input type="search" id="search-keyword" class="form-control search-field" name="search-keyword" placeholder="Keyword" >	
+		</div>		
+	</div>
+	<div class="row">		
+		<div class="form-group col-md-4">
+			<label for="search-category">Categories</label>
+			<select id="search-category" name="search-category" class="form-control search-field">
+				<option>All categories</option>
+				<option>Accounting / Finance</option>
+				<option>Administrative </option>
+				<option>Advertising / Media</option>
+				<option>Architecture</option>
+				<option>Art / Creative Design</option>
+				<option>Assistant / Secretary</option>
+				<option>Audit / Taxation</option>
+				<option>Automotive / Vehicle</option>
+				<option>Banking / Insurance</option>
+				<option>Catering / Fast food restaurant</option>
+			</select>
+		</div>
+		<div class="form-group col-md-4">
+			<label for="search-location">Locations</label>
+			<select id="search-location" name="search-location" class="form-control search-field">
+				<option>Phnom Penh</option>
+				<option>Siemreab</option>
+				<option>Sihanuk Ville</option>
+				<option>Kampong Chhnange</option>
+				<option>Preyveng</option>
+			</select>
+		</div>
+		<div class="form-group col-md-4">
+			<label for="search-salary">Expected salary</label>
+			<select id="search-salary" name="search-salary" class="form-control search-field">
+				<option>Minimum</option>
+				<option>$200</option>
+				<option>$500</option>
+				<option>$600</option>
+				<option>$1,000</option>
+				<option>$1,500</option>
+			</select>
+		</div>
+	</div>
+	<div class="row">		
+		<div class="form-group col-md-4">
+			<label for="search-industry">Industries</label>
+			<select id="search-industry" name="search-industry" class="form-control search-field">
+				<option>All industries</option>
+				<option>Accounting / Audit / Taxation</option>
+				<option>Advertising / Media / Publishing</option>
+				<option>Agriculture / Foresty</option>
+				<option>Architecture / Construction</option>
+				<option>Building / Engineering</option>
+				<option>Civil services</option>
+				<option>Cosmetic / Beauty</option>
+				<option>Clothing / Garmen</option>
+				<option>Energy / Oil / Gasoline</option>
+				<option>Entertainment</option>
+			</select>
+		</div>
+		<div class="form-group col-md-4">
+			<label for="search-worktime">Locations</label>
+			<select id="search-worktime" name="search-worktime" class="form-control search-field">
+				<option>Any time</option>
+				<option>Part time</option>
+				<option>Full time</option>
+			</select>
+		</div>
+		<div class="form-group col-md-4">
+			<label for="search-position">Expected salary</label>
+			<select id="search-position" name="search-position" class="form-control search-field">
+				<option value="1">Non-Executive</option>
+				<option value="2">Fresh/Entry Level</option>
+				<option value="3">Junior Executive</option>
+				<option value="4">Senior Executive</option>
+				<option value="5">Manager</option>
+				<option value="6">Senior Manager</option>
+				<option value="7">Top Level Management</option>
+			</select>
+		</div>
+	</div>
+	<div class="row">
+		<div class="form-group col-md-4">
+			<button type="submit" class="btn btn-default">Search</button>
+		</div>
+	</div>
+	@yield('body')
+</div>
