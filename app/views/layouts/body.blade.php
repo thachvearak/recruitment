ad2ea411709cf248f4381ae29939d86273a7ac22
cuoool <chea.bonnak@gmail.com>
2014-11-29 09:47:34 -0800

    Create Browse Jobs tab and update Search style

--- app/views/layouts/body.blade.php
+++ app/views/layouts/body.blade.php
@@ -2,8 +2,14 @@
 	<div id="search-block">
 		<div class="row">
 			<div class="form-group col-md-12">
-				<label for="search-keyword">Keywords</label>				
-				<input type="search" id="search-keyword" class="form-control search-field" name="search-keyword" placeholder="Keyword for job title, company name ... etc" >	
+				<label for="search-keyword">Keywords</label>					
+				
+				<div class="input-group">
+					<input type="search" id="search-keyword" class="form-control search-field" name="search-keyword" placeholder="Keyword for job title, company name ... etc" >
+					<span class="input-group-btn" >
+				  		<button type="submit" id="btn-search" class="btn btn-primary">Search</button>
+					</span>
+			    </div>
 			</div>		
 		</div>
 		<div class="row">		
@@ -83,9 +89,74 @@
 				</select>
 			</div>
 		</div>
+	</div>
+	
+	<div id="browse-jobs">
 		<div class="row">
-			<div class="form-group col-md-4">
-				<button type="submit" class="btn btn-default">Search</button>
+			<div class="col-md-12">			
+				<ul class="nav nav-tabs nav-justified" id="browse-jobs-tab">
+				  <li role="presentation" data-toggle="tab" class="active"><a href="#category">Categories</a></li>
+				  <li role="presentation" data-toggle="tab"><a href="#industry">Industries</a></li>
+				  <li role="presentation" data-toggle="tab"><a href="#location">Locations</a></li>
+				  <li role="presentation" data-toggle="tab"><a href="#salary">Salaries</a></li>
+				</ul>
+				<div class="tab-content">
+				  <div class="tab-pane active" id="category">
+				  		<ul class="inline">							 	
+							<li><span class="text"><a href="https://camupjob.com/category-1-accounting+financial">Accounting  / Financial</a></span> <span class="counter">(<em>80</em>)</span></li>			
+							<li><span class="text"><a href="https://camupjob.com/category-2-administrative">Administrative</a></span> <span class="counter">(<em>17</em>)</span></li>			
+							<li><span class="text"><a href="https://camupjob.com/category-8-advertising+media">Advertising / Media</a></span> <span class="counter">(<em>5</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-13-architecture">Architecture</a></span> <span class="counter">(<em>16</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-7-arts+creative-design">Arts / Creative Design</a></span> <span class="counter">(<em>5</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-4-assistant+secretary">Assistant / Secretary</a></span> <span class="counter">(<em>18</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-5-audit+taxation">Audit / Taxation</a></span> <span class="counter">(<em>1</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-46-automotive+vehicle">Automotive / Vehicle</a></span> <span class="counter">(<em>1</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-6-banking+insurance">Banking / Insurance</a></span> <span class="counter">(<em>17</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-9-catering+fast-food-restaurant">Catering / Fast food restaurant</a></span> <span class="counter">(<em>15</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-10-consultancy">Consultancy</a></span> <span class="counter">(<em>1</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-3-construction+engineering">Construction / Engineering</a></span> <span class="counter">(<em>7</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-37-cosmetic-services">Cosmetic Services</a></span> <span class="counter">(<em>5</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-36-customer-service">Customer Service</a></span> <span class="counter">(<em>21</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-16-education+teaching">Education / Teaching</a></span> <span class="counter">(<em>32</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-41-electronic+electrical+equipment">Electronic / Electrical / Equipment</a></span> <span class="counter">(<em>9</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-43-food-&amp;-beverages">Food &amp; Beverages</a></span> <span class="counter">(<em>7</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-15-freight+shipping+delivery">Freight / Shipping / Delivery</a></span> <span class="counter">(<em>15</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-44-general-business-services">General Business Services</a></span> <span class="counter">(<em>1</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-45-human-resources+consultant">Human Resources / Consultant</a></span> <span class="counter">(<em>7</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-11-it">IT</a></span> <span class="counter">(<em>40</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-12-lawyer+legal-services">Lawyer / Legal Services</a></span> <span class="counter">(<em>1</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-18-management">Management</a></span> <span class="counter">(<em>32</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-19-manufacturing">Manufacturing</a></span> <span class="counter">(<em>2</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-20-marketing">Marketing</a></span> <span class="counter">(<em>16</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-21-medical+health+nursing">Medical / Health / Nursing</a></span> <span class="counter">(<em>10</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-23-merchandising+purchasing">Merchandising / Purchasing</a></span> <span class="counter">(<em>10</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-25-operation">Operation</a></span> <span class="counter">(<em>1</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-26-project-management">Project Management</a></span> <span class="counter">(<em>2</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-14-property+real-estate">Property / Real Estate</a></span> <span class="counter">(<em>5</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-31-quality-control">Quality Control</a></span> <span class="counter">(<em>2</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-27-restaurant+hotel+casino">Restaurant / Hotel / Casino</a></span> <span class="counter">(<em>7</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-30-sale">Sale</a></span> <span class="counter">(<em>94</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-29-security-guard+driver">Security guard / Driver</a></span> <span class="counter">(<em>2</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-32-technician">Technician</a></span> <span class="counter">(<em>2</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-33-telecommunication">Telecommunication</a></span> <span class="counter">(<em>4</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-28-tourism+guide">Tourism / Guide</a></span> <span class="counter">(<em>4</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-42-trading+import-export">Trading / Import-Export</a></span> <span class="counter">(<em>1</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-34-translation+interpretation">Translation / Interpretation</a></span> <span class="counter">(<em>9</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-35-travel-agent+ticket-sales">Travel Agent / Ticket Sales</a></span> <span class="counter">(<em>2</em>)</span></li>				
+							<li><span class="text"><a href="https://camupjob.com/category-40-other">Other</a></span> <span class="counter">(<em>81</em>)</span></li>		
+						 </ul>
+				  </div>
+				  <div class="tab-pane" id="industry">ccccccccccc</div>
+				  <div class="tab-pane" id="location">ddddddd</div>
+				  <div class="tab-pane" id="salary">eeeeeeeeeeeee</div>
+				</div>			
+				
+				<script>
+					$('#browse-jobs-tab a').click(function (e) {
+						  e.preventDefault();
+						  $(this).tab('show');
+					});
+				</script>
 			</div>
 		</div>
 	</div>
