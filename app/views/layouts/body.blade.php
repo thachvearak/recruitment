fab9f76a0266dcdf38e7b0537ddecd1fdb46140d
cuoool <chea.bonnak@gmail.com>
2014-11-30 07:51:59 +0700

    Update Browse job list items and default search to Any.

--- app/views/layouts/body.blade.php
+++ app/views/layouts/body.blade.php
@@ -16,7 +16,7 @@
 			<div class="form-group col-md-4">
 				<label for="search-category">Categories</label>
 				<select id="search-category" name="search-category" class="form-control search-field">
-					<option>All categories</option>
+					<option>Any</option>
 					<option>Accounting / Finance</option>
 					<option>Administrative </option>
 					<option>Advertising / Media</option>
@@ -32,7 +32,7 @@
 			<div class="form-group col-md-4">
 				<label for="search-industry">Industries</label>
 				<select id="search-industry" name="search-industry" class="form-control search-field">
-					<option>All industries</option>
+					<option>Any</option>
 					<option>Accounting / Audit / Taxation</option>
 					<option>Advertising / Media / Publishing</option>
 					<option>Agriculture / Foresty</option>
@@ -48,6 +48,7 @@
 			<div class="form-group col-md-4">
 				<label for="search-location">Locations</label>
 				<select id="search-location" name="search-location" class="form-control search-field">
+					<option>Any</option>
 					<option>Phnom Penh</option>
 					<option>Siemreab</option>
 					<option>Sihanuk Ville</option>
@@ -60,7 +61,7 @@
 			<div class="form-group col-md-4">
 				<label for="search-salary">Expected salary</label>
 				<select id="search-salary" name="search-salary" class="form-control search-field">
-					<option>Minimum</option>
+					<option>Any</option>
 					<option>$200</option>
 					<option>$500</option>
 					<option>$600</option>
@@ -71,7 +72,7 @@
 			<div class="form-group col-md-4">
 				<label for="search-worktime">Working time</label>
 				<select id="search-worktime" name="search-worktime" class="form-control search-field">
-					<option>Any time</option>
+					<option>Any</option>
 					<option>Part time</option>
 					<option>Full time</option>
 				</select>
@@ -79,6 +80,7 @@
 			<div class="form-group col-md-4">
 				<label for="search-position">Position</label>
 				<select id="search-position" name="search-position" class="form-control search-field">
+					<option value="0">Any</option>
 					<option value="1">Non-Executive</option>
 					<option value="2">Fresh/Entry Level</option>
 					<option value="3">Junior Executive</option>
@@ -102,7 +104,7 @@
 				</ul>
 				<div class="tab-content">
 				  <div class="tab-pane active" id="category">
-				  		<ul class="inline">							 	
+				  		<ul>							 	
 							<li><span class="text"><a href="https://camupjob.com/category-1-accounting+financial">Accounting  / Financial</a></span> <span class="counter">(<em>80</em>)</span></li>			
 							<li><span class="text"><a href="https://camupjob.com/category-2-administrative">Administrative</a></span> <span class="counter">(<em>17</em>)</span></li>			
 							<li><span class="text"><a href="https://camupjob.com/category-8-advertising+media">Advertising / Media</a></span> <span class="counter">(<em>5</em>)</span></li>				
@@ -146,9 +148,327 @@
 							<li><span class="text"><a href="https://camupjob.com/category-40-other">Other</a></span> <span class="counter">(<em>81</em>)</span></li>		
 						 </ul>
 				  </div>
-				  <div class="tab-pane" id="industry">ccccccccccc</div>
-				  <div class="tab-pane" id="location">ddddddd</div>
-				  <div class="tab-pane" id="salary">eeeeeeeeeeeee</div>
+				  <div class="tab-pane" id="industry">
+				  		<ul>
+
+							 	
+									<li><span class="text"><a href="https://camupjob.com/industry-2-advertising+media+publishing">Advertising / Media / Publishing</a></span> <span class="counter">(<em>1</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-5-architecture+construction">Architecture / Construction</a></span> <span class="counter">(<em>9</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-7-banking+financial">Banking / Financial</a></span> <span class="counter">(<em>9</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-11-clothing+garment">Clothing / Garment</a></span> <span class="counter">(<em>18</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-12-cosmetics+beauty">Cosmetics / Beauty</a></span> <span class="counter">(<em>3</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-13-education">Education</a></span> <span class="counter">(<em>32</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-43-energy+oil+gasoline">Energy / Oil / Gasoline</a></span> <span class="counter">(<em>5</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-15-entertainment">Entertainment</a></span> <span class="counter">(<em>4</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-16-food-&amp;-beverages">Food &amp; Beverages</a></span> <span class="counter">(<em>15</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-17-freight+shipping+delivery">Freight / Shipping / Delivery</a></span> <span class="counter">(<em>4</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-18-general-business-services">General Business Services</a></span> <span class="counter">(<em>10</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-19-hospital+pharmacy">Hospital / Pharmacy</a></span> <span class="counter">(<em>3</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-44-hotel+accommodation">Hotel / Accommodation</a></span> <span class="counter">(<em>15</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-20-human-resources+consultant">Human Resources / Consultant</a></span> <span class="counter">(<em>49</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-21-industrial-machinery">Industrial Machinery</a></span> <span class="counter">(<em>5</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-22-information-technology">Information Technology</a></span> <span class="counter">(<em>4</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-23-insurance">Insurance</a></span> <span class="counter">(<em>6</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-27-manufacturing">Manufacturing</a></span> <span class="counter">(<em>11</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-28-ngo+social-services">NGO / Social Services</a></span> <span class="counter">(<em>10</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-42-other">Other</a></span> <span class="counter">(<em>35</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-31-property-management">Property Management</a></span> <span class="counter">(<em>10</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-32-real-estate">Real Estate</a></span> <span class="counter">(<em>5</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-33-recruiting-services">Recruiting Services</a></span> <span class="counter">(<em>331</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-35-sports-&amp;-recreation">Sports &amp; Recreation</a></span> <span class="counter">(<em>9</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-37-telecommunication">Telecommunication</a></span> <span class="counter">(<em>5</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-38-tourism">Tourism</a></span> <span class="counter">(<em>5</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-39-trading">Trading</a></span> <span class="counter">(<em>2</em>)</span></li>
+					
+									<li><span class="text"><a href="https://camupjob.com/industry-41-wholesale+retail">Wholesale / Retail</a></span> <span class="counter">(<em>5</em>)</span></li>
+					
+
+							 </ul>
+				  </div>
+				  <div class="tab-pane" id="location">
+				  		<ul>
+
+							 	
+
+									<li><span class="text"><a href="https://camupjob.com/location-1-phnom-penh">Phnom Penh</a></span> <span class="counter">(<em>576</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-2-province">Provinces</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-12-siem-reap">Siem Reap</a></span> <span class="counter">(<em>28</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-13-preah-sihanouk">Preah Sihanouk</a></span> <span class="counter">(<em>8</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-14-banlung">Banlung</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-15-banteay-meanchey">Banteay Meanchey</a></span> <span class="counter">(<em>4</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-16-battambang">Battambang</a></span> <span class="counter">(<em>6</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-17-bavet">Bavet</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-18-kampong-cham">Kampong Cham</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-19-kampong-chhnang">Kampong Chhnang</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-20-kampong-speu">Kampong Speu</a></span> <span class="counter">(<em>10</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-21-kampong-thom">Kampong Thom</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-22-kampot">Kampot</a></span> <span class="counter">(<em>5</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-23-kandal">Kandal</a></span> <span class="counter">(<em>14</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-24-kep">Kep</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-25-koh-kong">Koh Kong</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-26-takhmao">Takhmao</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-27-kratie">Kratie</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-28-mondulkiri">Mondulkiri</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-29-oddor-meanchey">Oddor Meanchey</a></span> <span class="counter">(<em>4</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-30-pailin">Pailin</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-31-poipet">Poipet</a></span> <span class="counter">(<em>2</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-32-preah-vihear">Preah Vihear</a></span> <span class="counter">(<em>2</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-33-prey-veng">Prey Veng</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-34-pursat">Pursat</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-35-rattanakiri">Rattanakiri</a></span> <span class="counter">(<em>6</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-36-sre-ambel">Sre Ambel</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-37-stung-treng">Stung Treng</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-38-svay-rieng">Svay Rieng</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-39-takeo">Takeo</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/location-40-tabung-khum">Tabung Khum</a></span> <span class="counter">(<em>0</em>)</span></li>
+
+					
+
+							 </ul>
+				  </div>
+				  <div class="tab-pane" id="salary">
+				  		<ul>
+
+							 	
+
+									<li><span class="text"><a href="https://camupjob.com/salary-4000">4000 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-2000">2000 ~ Up</a></span> <span class="counter">(<em>4</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-1800">1800 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-1500">1500 ~ Up</a></span> <span class="counter">(<em>2</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-1200">1200 ~ Up</a></span> <span class="counter">(<em>9</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-1000">1000 ~ Up</a></span> <span class="counter">(<em>14</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-800">800 ~ Up</a></span> <span class="counter">(<em>5</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-750">750 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-700">700 ~ Up</a></span> <span class="counter">(<em>10</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-650">650 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-600">600 ~ Up</a></span> <span class="counter">(<em>8</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-500">500 ~ Up</a></span> <span class="counter">(<em>39</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-450">450 ~ Up</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-400">400 ~ Up</a></span> <span class="counter">(<em>18</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-350">350 ~ Up</a></span> <span class="counter">(<em>5</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-320">320 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-300">300 ~ Up</a></span> <span class="counter">(<em>37</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-250">250 ~ Up</a></span> <span class="counter">(<em>17</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-200">200 ~ Up</a></span> <span class="counter">(<em>62</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-180">180 ~ Up</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-160">160 ~ Up</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-150">150 ~ Up</a></span> <span class="counter">(<em>34</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-140">140 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-130">130 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-120">120 ~ Up</a></span> <span class="counter">(<em>11</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-110">110 ~ Up</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-100">100 ~ Up</a></span> <span class="counter">(<em>7</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-90">90 ~ Up</a></span> <span class="counter">(<em>1</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-80">80 ~ Up</a></span> <span class="counter">(<em>3</em>)</span></li>
+
+					
+
+									<li><span class="text"><a href="https://camupjob.com/salary-Negotiable">Negotiable</a></span> <span class="counter">(<em>327</em>)</span></li>
+
+					
+
+							 </ul>
+				  </div>
 				</div>			
 				
 				<script>
