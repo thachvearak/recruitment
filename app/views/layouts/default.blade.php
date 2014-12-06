9f745cca16c359c2c8247c0c6ae3ebeb8ee9579b
Bonnak <chea.bonnak@gmail.com>
2014-12-07 06:41:20 +0700

    Add font-awesome to the project

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -11,6 +11,8 @@
 	href="{{asset('bootstrap-3.3.1/css/bootstrap.min.css')}}">
 <link rel="stylesheet"
 	href="{{asset('bootstrap-3.3.1/css/bootstrap-theme.min.css')}}">
+	<link rel="stylesheet"
+	href="{{asset('font-awesome-4.2.0/css/font-awesome.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/global.css')}}">
 
 <!-- JS -->
