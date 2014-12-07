d9916fb1501fbb5a12c74b1215885f45dbf1286c
Bonnak <chea.bonnak@gmail.com>
2014-12-07 07:26:35 +0700

    update footer, add follw

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,15 +1,18 @@
 <div class="container footer">
 	<div class="row">
-		<div class="col-md-6">
-			<address>
-				<h1 style="margin-top: 0;">Aitie Agency.</h1>
-				<span class="address">795 Folsom Ave, Suite 600, San Francisco, CA
-					94107</span><br> <span class="phone">Call us <b>(123) 456-7890</b></span><br>
-				<span class="email">Email us <b>first.last@example.com</b></span><br>
-			</address>
+		<div class="col-md-3">
+				<h1 style="margin-top: 0;">Aitie Agency.</h1>				
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-5">			
+			<div class="contact">
+				<span class="address">795 Folsom Ave, Suite 600, San Francisco, CA
+					94107. </span><br><span class="phone">Call us <b>(123) 456-7890</b></span>
+				<span class="email">Email us <b>first.last@example.com</b></span>
+			</div>	
+		</div>
+		<div class="col-md-4">
 			<div id="follow">
+				<span>Keep intouch with us </span>
 					<ul>
 						<li><a target="_blank" class="fb"></a></li>
 						<li><a target="_blank" class="tw"></a></li>
