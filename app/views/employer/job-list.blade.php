d9bc95f130bb2ced5680c98387706b16b3dff547
vearak <thachvearak@gmail.com>
2015-04-26 18:26:37 +0700

    Add form tage

--- app/views/employer/job-list.blade.php
+++ app/views/employer/job-list.blade.php
@@ -5,32 +5,34 @@
 <div id="employer" class="middle-wrapper pull-left">
 	<div id="job-list">
 		<div class="title-bar">Jobs List</div>
-		<div class="content-wrapper">
-			<table class="table table-condensed table-bordered">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Published Date</th>
-						<th>Closing Date</th>
-						<th>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach($jobs as $job)
-					<tr>
-						<td>{{$job->title}}</td>
-						<td>{{$job->published_date}}</td>
-						<td>{{$job->closing_date}}</td>
-						<td>
-							<a href="">Edit</a><br>
-							<a href="">Delete</a><br> 
-							<a href="">Preview</a>
-						</td>
-					</tr>
-					@endforeach
-				</tbody>
-			</table>
-		</div>
+		<form>
+			<div class="content-wrapper">
+				<table class="table table-condensed table-bordered">
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Published Date</th>
+							<th>Closing Date</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($jobs as $job)
+						<tr>
+							<td>{{$job->title}}</td>
+							<td>{{$job->published_date}}</td>
+							<td>{{$job->closing_date}}</td>
+							<td>
+								<a href="">Edit</a><br>
+								<a href="">Delete</a><br> 
+								<a href="">Preview</a>
+							</td>
+						</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</form>
 	</div>
 </div>
 <div class="right-side-bar pull-left"></div>
